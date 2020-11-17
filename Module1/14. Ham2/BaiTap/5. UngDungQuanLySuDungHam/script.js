1ebc86cdacbcaca4d9ae649bcf01e94e5d2241c1
Phuc Pham <truongphucdn@gmail.com>
2020-11-17 08:27:21 +0700

    17.11

--- Module1/14. Ham2/BaiTap/5. UngDungQuanLySuDungHam/script.js
+++ Module1/14. Ham2/BaiTap/5. UngDungQuanLySuDungHam/script.js
@@ -1,45 +1,76 @@
-let arrayProduct = [];
-let tableProduct = document.getElementById("table");
+let productArray = [];
+
+let productInput = document.getElementById("product-input");
 let addButton = document.getElementById("add");
-let inputProduct = document.getElementById("product-input");
-let count = document.getElementById("count");
+let bodyTable = document.getElementById("body-table");
 
 addButton.addEventListener("click", addProduct);
 
+
 function addProduct() {
-    if (inputProduct.value) {
-        arrayProduct.push();
-        tableProduct.innerHTML += `<tr class="row" id="row-${arrayProduct.length}">
-                                       <td id="text-${arrayProduct.length}">${inputProduct.value}</td>
-                                       <td><button id="edit-${arrayProduct.length}">Edit</button></td>
-                                       <td><button id="remove-${arrayProduct.length}">Remove</button></td>
-                                   </tr>`;
+    if (productInput.value) {
+        productArray.push(productInput.value);
     }
-    inputProduct.value = "";
-    document.getElementById(`edit-${arrayProduct.length}`).addEventListener("click",editProduct);
-    document.getElementById(`remove-${arrayProduct.length}`).addEventListener("click",removeProduct);
-    updateCount();
+    generateTable(productArray);
+    productInput.value = "";
 }
 
 function editProduct() {
-    let id = parseInt(this.id);
-    document.getElementById(`text-${id}`).innerHTML = `<input id="input-${id}" type="text"/>`;
+    let id = this.id.split("-")[1];
+    document.getElementById(`edit-${id}`).removeEventListener("click",editProduct);
     document.getElementById(`edit-${id}`).innerText = "Confirm";
-    document.getElementById(`edit-${id}`).addEventListener("click",updateProduct);
+    document.getElementById(`displayName-${id}`).contentEditable = "true";
+    document.getElementById(`edit-${id}`).addEventListener("click",confirmEdit);
 }
-
-function removeProduct() {
-    updateCount();
-}
-
-function updateProduct() {
-    let id = parseInt(this.id);
-
-    if(document.getElementById((`input-${id}`).value){
+function confirmEdit() {
+    let id = this.id.split("-")[1];
+    document.getElementById(`edit-${id}`).removeEventListener("click",confirmEdit);
     document.getElementById(`edit-${id}`).innerText = "Edit";
-    document.getElementById(this.id).addEventListener("click",editProduct);
-        {
+    document.getElementById(`displayName-${id}`).contentEditable = "false";
+    document.getElementById(`edit-${id}`).addEventListener("click",editProduct);
+    productArray[id] = document.getElementById(`displayName-${id}`).innerText;
 }
-function updateCount() {
+function removeProduct() {
+    let id = this.id.split("-")[1];
+    productArray.splice(id,1);
+    generateTable(productArray);
+}
 
+function generateTable(arr) {
+    while (bodyTable.hasChildNodes()) {
+        bodyTable.removeChild(bodyTable.lastChild)
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let row = bodyTable.insertRow();
+        row.id = `row-${i}`;
+
+        //product name
+        let td_1 = document.createElement("td");
+        td_1.innerText = arr[i];
+        td_1.id = `displayName-${i}`;
+        row.appendChild(td_1);
+
+        //button edit
+
+        let td_2 = document.createElement("td");
+        let button_edit = document.createElement("button")
+        button_edit.innerText = "Edit";
+        button_edit.id = `edit-${i}`;
+        button_edit.addEventListener("click",editProduct);
+        row.appendChild(td_2);
+        td_2.appendChild(button_edit);
+
+        //button remove
+
+        let td_3 = document.createElement("td");
+        let button_remove = document.createElement("button")
+        button_remove.innerText = "Remove";
+        button_remove.id = `remove-${i}`;
+        button_remove.addEventListener("click",removeProduct);
+        row.appendChild(td_3);
+        td_3.appendChild(button_remove);
+
+    }
+    document.getElementById('count').innerText = arr.length + ((arr.length >1)? " Products": " Product");
 }

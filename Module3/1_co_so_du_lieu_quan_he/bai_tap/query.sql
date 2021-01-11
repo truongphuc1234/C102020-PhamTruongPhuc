4e80f6627bfb8d2623c447c617324556a8acb212
Phuc Pham <truongphucdn@gmail.com>
2021-01-11 07:18:30 +0700

    11.01

---
+++ Module3/1_co_so_du_lieu_quan_he/bai_tap/query.sql
@@ -0,0 +1,7 @@
+USE classicmodels;
+
+SELECT * FROM customers;
+SELECT customerName, phone, city, country FROM customers;
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique';
+SELECT * FROM customers WHERE customername like '%A%';
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC');
